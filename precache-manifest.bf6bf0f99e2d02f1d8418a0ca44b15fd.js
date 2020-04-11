6b3d32d5b5c2d49a5b4ac1122ecc7d2318f16b89
Kevin Hu <hu.kevin.w@gmail.com>
2020-04-11 17:05:14 -0400

    Updates

---
+++ precache-manifest.bf6bf0f99e2d02f1d8418a0ca44b15fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe42f4c732edceef3031c6e70a2883c",
+    "url": "/react-github-pages-boilerplate/index.html"
+  },
+  {
+    "revision": "96d4644107f6d355f169",
+    "url": "/react-github-pages-boilerplate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f542c4872c3a66bee6ab",
+    "url": "/react-github-pages-boilerplate/static/js/2.313b8a54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-pages-boilerplate/static/js/2.313b8a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d4644107f6d355f169",
+    "url": "/react-github-pages-boilerplate/static/js/main.403953de.chunk.js"
+  },
+  {
+    "revision": "c0ac7f962cbb7d159e54",
+    "url": "/react-github-pages-boilerplate/static/js/runtime-main.2ce31dfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-pages-boilerplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
